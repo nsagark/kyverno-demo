da94cb23e76d868a8d30fd1ed7316c4f41ca964c
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-07 17:24:35 -0500

    Update install-kycli.sh

--- install-kycli.sh
+++ install-kycli.sh
@@ -7,3 +7,13 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 go version
+
+git clone https://github.com/kyverno/kyverno
+
+cd kyverno
+
+make cli
+
+mv ./cmd/cli/kubectl-kyverno/kyverno /usr/local/bin/kyverno
+
+kyverno version
