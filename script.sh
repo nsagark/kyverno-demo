4c2cbe892f610b58a1c656093b6f647f67602fa0
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-08 19:14:16 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -24,8 +24,8 @@ echo
 
 #printf "%-50s %-20s %-20s %-20s %-20s %-20s\n" "RESOURCE_NAME" "KIND" "NAMESPACE" "POLICY_YAML_FILE" "RESOURCE_YAML_FILE"
 
-echo -e "The resources that have failed the kyverno policies are listed below along with the policy and resource YAML files.\n\nNOTE: The RESOURCE below has the following format\n\nNAMESPACE/KIND/RESOURCE_NAME"
-seq -s- 100|tr -d '[:digit:]'
+echo -e "The resources that have failed the kyverno policies are listed below along with the policy and resource YAML files.\nNOTE: The RESOURCE below has the following format\nNAMESPACE/KIND/RESOURCE_NAME"
+
 printf "%-50s %-30s %-30s\n" "RESOURCE" "POLICY_YAML_FILE" "RESOURCE_YAML_FILE"
 seq -s- 100|tr -d '[:digit:]'
 
