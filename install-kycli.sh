c951f4ac39423d3cca8ea2fe4553c375d1a8069c
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-08 19:26:16 -0500

    Update install-kycli.sh

--- install-kycli.sh
+++ install-kycli.sh
@@ -4,7 +4,7 @@ apt install make
 
 wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz 2>&1 > /dev/null
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 
 export PATH=$PATH:/usr/local/go/bin
 
