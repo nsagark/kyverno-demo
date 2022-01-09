05e26054662834c0a37fc5bb90f2d3199885e9fe
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-09 06:51:49 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -48,7 +48,7 @@ if [[ -s $TEMP_FILE ]]; then
         done < $TEMP_FILE
         cat $OUTPUT_FILE
         EMAIL_BODY="The resource YAML files that have failed to validate against the kyverno policies are included in the attachment. Please take appropriate actions to update the YAML files"
-        mailx -s "Pipeline job has failed" -a $OUTPUT_FILE -- $E_DISTRO <<< "$EMAIL_BODY"
+        mutt -s "Pipeline job has failed" -a $OUTPUT_FILE -- $E_DISTRO <<< "$EMAIL_BODY"
         echo 
         echo
         exit 1
