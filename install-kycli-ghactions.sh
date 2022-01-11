35293520e13581c03936f4ef99ce21011ebb2168
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-11 08:37:02 -0500

    Create install-kycli-ghactions.sh

---
+++ install-kycli-ghactions.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+apt install make
+
+wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+go version
+
+git clone https://github.com/kyverno/kyverno
+
+cd kyverno
+
+make cli
+
+mv ./cmd/cli/kubectl-kyverno/kyverno /usr/local/bin/kyverno
+echo
+echo -e "\nPrinting kyverno version below: "
+kyverno version
