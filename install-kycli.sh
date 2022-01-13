80fb7adb03a9d4c43b662817318c5368677cd890
nsagark <90008930+nsagark@users.noreply.github.com>
2022-01-13 06:30:44 -0500

    Update install-kycli.sh

--- install-kycli.sh
+++ install-kycli.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/bash
 
-apt install make
+kycli_version=$(kyverno version | grep Version | awk '{ print $NF}')
 
-wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
+if [[ -z $kycli_version ]]; then
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+  apt install make
+  wget https://go.dev/dl/go1.17.6.linux-amd64.tar.gz
+  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+  export PATH=$PATH:/usr/local/go/bin
+  go version
+  git clone https://github.com/kyverno/kyverno
+  cd kyverno
+  make cli
+  mv ./cmd/cli/kubectl-kyverno/kyverno /usr/local/bin/kyverno
+  echo
+  echo -e "\nPrinting kyverno version below: "
+  kyverno version
 
-export PATH=$PATH:/usr/local/go/bin
-
-go version
-
-git clone https://github.com/kyverno/kyverno
-
-cd kyverno
-
-make cli
-
-mv ./cmd/cli/kubectl-kyverno/kyverno /usr/local/bin/kyverno
-echo
-echo -e "\nPrinting kyverno version below: "
-kyverno version
+fi
